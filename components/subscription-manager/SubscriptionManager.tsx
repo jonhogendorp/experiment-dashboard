53de0d609e4976cb1f75825c17771ee854bbfe41
Jon Hogendorp <jonhoog@live.nl>
2025-06-13 16:56:29 +0200

    feat: enhance category buttons in SubscriptionManager with improved styling and icons

--- components/subscription-manager/SubscriptionManager.tsx
+++ components/subscription-manager/SubscriptionManager.tsx
@@ -113,16 +113,20 @@ export default function SubscriptionManager() {
 			{/* category picker */}
 			<div className='my-4 border-t pt-4 w-full flex flex-col items-center'>
 				<div className='flex flex-row gap-2'>
-					<button className='px-4 py-2 rounded-full bg-blue-100 text-blue-700 font-semibold hover:bg-blue-200 transition'>
+					<button className='px-4 py-2 rounded-full flex flex-row items-center gap-2 bg-slate-200 text-slate-700 font-semibold hover:bg-slate-300 transition'>
+						<div className='bg-blue-500 h-3 w-3 rounded-full'></div>
 						Streaming
 					</button>
-					<button className='px-4 py-2 rounded-full bg-green-100 text-green-700 font-semibold hover:bg-green-200 transition'>
+					<button className='px-4 py-2 rounded-full flex flex-row items-center gap-2 bg-slate-200 text-slate-700 font-semibold hover:bg-slate-300 transition'>
+						<div className='bg-green-500 h-3 w-3 rounded-full'></div>
 						Music
 					</button>
-					<button className='px-4 py-2 rounded-full bg-yellow-100 text-yellow-700 font-semibold hover:bg-yellow-200 transition'>
+					<button className='px-4 py-2 rounded-full flex flex-row items-center gap-2  bg-slate-200 text-slate-700 font-semibold hover:bg-slate-300 transition'>
+						<div className='bg-yellow-400 h-3 w-3 rounded-full'></div>
 						Productivity
 					</button>
-					<button className='px-4 py-2 rounded-full bg-purple-100 text-purple-700 font-semibold hover:bg-purple-200 transition'>
+					<button className='px-4 py-2 rounded-full flex flex-row items-center gap-2  bg-slate-200 text-slate-700 font-semibold hover:bg-slate-300 transition'>
+						<div className='bg-purple-500 h-3 w-3 rounded-full'></div>
 						Other
 					</button>
 				</div>
