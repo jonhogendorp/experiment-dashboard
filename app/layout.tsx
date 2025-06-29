6db5dd593ae95bb0764b97d070b6715821145678
Jon Hogendorp <64735813+jonhogendorp@users.noreply.github.com>
2025-06-29 23:55:32 +0200

    feat: refactor ApolloProvider usage into a dedicated wrapper component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,7 @@
 import type { Metadata } from "next";
-import { ApolloProvider } from "@apollo/client";
-import client from "@/lib/apolloClient";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import ApolloProviderWrapper from "@/components/ApolloProviderWrapper";
 
 const geistSans = Geist({
 	variable: "--font-geist-sans",
@@ -29,7 +28,7 @@ export default function RootLayout({
 			<body
 				className={`${geistSans.variable} ${geistMono.variable} antialiased`}
 			>
-				<ApolloProvider client={client}>{children}</ApolloProvider>
+				<ApolloProviderWrapper>{children}</ApolloProviderWrapper>
 			</body>
 		</html>
 	);
